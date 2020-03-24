c33f2ffe06aebd63085d5cb56fe61e2d96812929
Kyle Sweigart <kasweigart@gmail.com>
2020-03-24 15:25:49 -0400

    css flexbox done

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-let container = document.getElementsByClassName('container');
-let gridRow = document.getElementsByClassName('grid-row');
-let gridCell = document.getElementsByClassName('grid-cell');
+let container = document.querySelector(".container");
+let clearGridBtn = document.querySelector(".clearGrid");
+
+clearGridBtn.addEventListener('click', clearGrid);
+
+
+function createGrid(size) {
+    for (i=0; i<size; i++) {
+        let createRow = document.createElement('div');
+        createRow.classList.add('grid-row');
+        for (x=0; x< size; x++) {
+            let createCell = document.createElement('div');
+            createCell.classList.add('grid-cell');
+            createCell.innerText = 'test';
+            createRow.appendChild(createCell);
+        }
+    container.appendChild(createRow);
+    }
+    
+}
+
+function clearGrid() {
+    prompt('Enter your new grid size. (16-128)');
+}
+
+createGrid(16);
