774d2a55c27b98cfd8cd004ac9d1b190386586d4
Kyle Sweigart <kasweigart@gmail.com>
2020-03-25 10:10:24 -0400

    added function to change board size

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 let container = document.querySelector(".container");
 let clearGridBtn = document.querySelector(".clearGrid");
 
-clearGridBtn.addEventListener('click', clearGrid);
-
+clearGridBtn.addEventListener("click", clearGrid);
 
 function createGrid(size) {
-    for (i=0; i<size; i++) {
-        let createRow = document.createElement('div');
-        createRow.classList.add('grid-row');
-        for (x=0; x< size; x++) {
-            let createCell = document.createElement('div');
-            createCell.classList.add('grid-cell');
-            createCell.innerText = 'test';
-            createRow.appendChild(createCell);
-        }
-    container.appendChild(createRow);
+  for (i = 0; i < size; i++) {
+    let createRow = document.createElement("div");
+    createRow.classList.add("grid-row");
+    for (x = 0; x < size; x++) {
+      let createCell = document.createElement("div");
+      createCell.classList.add("grid-cell");
+      createRow.appendChild(createCell);
     }
-    
+    container.appendChild(createRow);
+  }
+  draw();
 }
-
-function clearGrid() {
-    prompt('Enter your new grid size. (16-128)');
-}
-
 createGrid(16);
+function draw() {
+  let newCell = document.querySelectorAll(".grid-cell");
+  [...newCell].forEach(cell =>
+    cell.addEventListener("mouseover", () => {
+      cell.className = "fill";
+    })
+  );
+}
+function clearGrid() {
+  let entry = prompt("Enter your new grid size. (16-128)");
+  let child = container.lastElementChild;
+  while (child) {
+      container.removeChild(child);
+      child = container.lastElementChild;
+  }
+  createGrid(entry);
+}
